ea4ef25f91e144e58add2aa6de7323d7f4a3578f
Alex2269 <alekader@ramber.ru>
2020-12-11 12:12:57 +0200

    2020-12-11

--- stm32f103-ll-3-phase-pwm/Core/Src/main.c
+++ stm32f103-ll-3-phase-pwm/Core/Src/main.c
@@ -50,30 +50,28 @@
 
 /* USER CODE BEGIN PV */
 
-float PI = 3.14;
-float delay_time = 2000.0; // set delay for begin minimal speed
-float prescaler = 80.0; // set delay for begin minimal speed
-//
-//
-volatile uint8_t index_a_sin = 0; // Индекс фазы A в таблице синуса
-volatile uint8_t index_b_sin = 85; // Индекс фазы B в таблице синуса
-volatile uint8_t index_c_sin = 43; // Индекс фазы C в таблице синуса
-volatile uint8_t temp_sin; // служебная переменная 1 байт
-volatile uint8_t temp_a; // временное хранение значения фазы A
-volatile uint8_t temp_b; // временное хранение значения фазы B
-volatile uint8_t temp_c; // временное хранение значения фазы C
-volatile uint8_t pol_a = 1; // полярность фазы A "1" - больше 126
-volatile uint8_t pol_b = 1; // полярность фазы B "0" - меньше 128
-volatile uint8_t pol_c = 0; // полярность фазы C
-volatile uint8_t index_bc_calc = 1; // "1" - calculate indexes for phases B & C
-volatile uint8_t inc_ind = 1; // Шаг индекса в таблице синуса
-volatile float amp_sin = 0; // амплитуда синуса. 255 это максимум.
-
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 /* USER CODE BEGIN PFP */
+
+float PI = 3.14;
+float delay_time = 2000.0; // set delay for begin minimal speed
+float prescaler = 80.0; // set delay for begin minimal speed
+volatile uint8_t index_a_sin = 0; // phase index A in the sine table
+volatile uint8_t index_b_sin = 85; // phase index B in the sine table
+volatile uint8_t index_c_sin = 43; // phase index C in the sine table
+volatile uint8_t temp_sin; // service variable
+volatile uint8_t temp_a; // temporary value storage for phase A
+volatile uint8_t temp_b; // temporary value storage for phase B
+volatile uint8_t temp_c; // temporary value storage for phase C
+volatile uint8_t pol_a = 1; // polarity phase A "1" - it is more 126
+volatile uint8_t pol_b = 1; // polarity phase B "0" - it is less 128
+volatile uint8_t pol_c = 0; // polarity phase C
+volatile uint8_t index_bc_calc = 1; // "1" - calculate indexes for phases B & C
+volatile uint8_t inc_ind = 1; // index step in the sine table
+volatile float amp_sin = 0; // sinus amplitude 255 the max.
 
 void delay_cycle(volatile uint32_t cycle_count)
 {
@@ -85,45 +83,41 @@ void delay_cycle(volatile uint32_t cycle_count)
 
 void timer_pwm_init(void)
 {
-
   TIM1->ARR = AutoReload; // set auto-reload register
-
-  TIM1->CCER |= TIM_CCER_CC1E;  // LL_TIM_CC_EnableChannel(TIM1, LL_TIM_CHANNEL_CH1);   // start PWM on CH1 pin
-  TIM1->CCER |= TIM_CCER_CC2E;  // LL_TIM_CC_EnableChannel(TIM1, LL_TIM_CHANNEL_CH2);   // start PWM on CH2 pin
-  TIM1->CCER |= TIM_CCER_CC3E;  // LL_TIM_CC_EnableChannel(TIM1, LL_TIM_CHANNEL_CH3);   // start PWM on CH3 pin
-  TIM1->CCER |= TIM_CCER_CC1NE; // LL_TIM_CC_EnableChannel(TIM1, LL_TIM_CHANNEL_CH1N);  // start PWM on CH1N pin
-  TIM1->CCER |= TIM_CCER_CC2NE; // LL_TIM_CC_EnableChannel(TIM1, LL_TIM_CHANNEL_CH2N);  // start PWM on CH2N pin
-  TIM1->CCER |= TIM_CCER_CC3NE; // LL_TIM_CC_EnableChannel(TIM1, LL_TIM_CHANNEL_CH3N);  // start PWM on CH3N pin
-
+  TIM1->CCER |= TIM_CCER_CC1E;  // LL_TIM_CC_EnableChannel(TIM1, LL_TIM_CHANNEL_CH1);   // start pwm on CH1 pin
+  TIM1->CCER |= TIM_CCER_CC2E;  // LL_TIM_CC_EnableChannel(TIM1, LL_TIM_CHANNEL_CH2);   // start pwm on CH2 pin
+  TIM1->CCER |= TIM_CCER_CC3E;  // LL_TIM_CC_EnableChannel(TIM1, LL_TIM_CHANNEL_CH3);   // start pwm on CH3 pin
+  TIM1->CCER |= TIM_CCER_CC1NE; // LL_TIM_CC_EnableChannel(TIM1, LL_TIM_CHANNEL_CH1N);  // start pwm on CH1N pin
+  TIM1->CCER |= TIM_CCER_CC2NE; // LL_TIM_CC_EnableChannel(TIM1, LL_TIM_CHANNEL_CH2N);  // start pwm on CH2N pin
+  TIM1->CCER |= TIM_CCER_CC3NE; // LL_TIM_CC_EnableChannel(TIM1, LL_TIM_CHANNEL_CH3N);  // start pwm on CH3N pin
   TIM1->CR1 = TIM_CR1_CEN; //  LL_TIM_EnableCounter(TIM1);
-
 }
 
 void spin(void)
 {
   for(int32_t i=0;i<sinus_points;i++)
   {
-    prescaler -= 0.01; // повышение частоты таймера
+    prescaler -= 0.01; // increasing timer frequency
     if(prescaler < 40) prescaler = 40; // limit prescaler
     // --
-    delay_time -= 0.1; // уменьшение периода (раскрутка)
+    delay_time -= 0.1; // reduction of the period (spindle acceleration)
     if(delay_time < sinus_points) delay_time = sinus_points; // limit delay_time
     // --
     amp_sin += 0.025; // прирост амплитуды
     if(amp_sin>254) amp_sin=255; // limt amplitude
     // --
     // ============================================
-    // Вычисления для Фазы А
+    // calculating for the phase A
     // index_a_sin += inc_ind;
     // ============================================
-    // Перекл. полярн. Фазы А и вычисление индекса
+    // switching phase Polarity А and calculate index
     if (index_a_sin > 127)
     {
       index_a_sin -= 128;
       index_bc_calc = 0; // no calculate indexes for phases B & C
       if (pol_a)
       {
-        // Переключение полярности Фаз
+        // switching phase Polarity
         pol_a = 0; pol_b = 0; pol_c = 1;
         index_b_sin = index_a_sin + 85;
         index_c_sin = index_a_sin + 43;
@@ -136,17 +130,17 @@ void spin(void)
       }
     }
     // ============================================
-    // Проверка индексов для Фаз B и C и полярности
+    // checking indexes for phases B and C and polarity
     if (index_bc_calc)
     {
       // ============================================
-      // Вычисл. индекса Фазы B и полярности
+      // calculating the phase index B and polarity
       if (index_b_sin > 127)
       {
         index_b_sin -= 128;
         if (pol_b)
         {
-          // Переключение полярности Фазы B
+          // switching phase polarity B
           pol_b = 0;
         }
         else
@@ -155,13 +149,13 @@ void spin(void)
         }
       }
       // ============================================
-      // Вычисл. индекса Фазы С и полярности
+      // calculating the phase index С and polarity
       if (index_c_sin > 127)
       {
         index_c_sin -= 128;
         if (pol_c)
         {
-          // Переключение полярности Фазы B
+          // switching phase polarity B
           pol_c = 0;
         }
         else
@@ -171,59 +165,59 @@ void spin(void)
       }
     }
     // ============================================
-    // ТУТ индексы всех фах известны и полярности тоже.
+    // here the indices of all fach are known and polarities too.
     // ============================================
-    // Вычисление амплитуды Фазы А
-    // Умножаем значение из таблицы на коэф. amp_sin и берем старший байт.
+    // calculation of phase amplitude А
+    // multiply the value from the table by coefficient amp_sin and we take the most significant byte.
     temp_sin = (((int16_t)amp_sin * (int16_t)sinus[index_a_sin])) >> 8;
     // --
     if (pol_a)
     {
-      // если положительная полуволна Фазы
+      // if positive half wave phase
       temp_a = temp_sin + 127;
     }
     else
     {
-      // если отрицательная полуволна Фазы
+      // if negative half wave phase
       temp_a = 127 - temp_sin;
     }
     // ============================================
-    // Вычисление амплитуды Фазы B
-    // Умножаем значение из таблицы на коэф. amp_sin и берем старший байт.
+    // calculation of phase amplitude B
+    // multiply the value from the table by coefficient amp_sin and we take the most significant byte.
     temp_sin = (((int16_t)amp_sin * (int16_t)sinus[index_b_sin])) >> 8;
     // --
     if (pol_b)
     {
-      // если положительная полуволна Фазы
+      // if positive half wave phase
       temp_b = temp_sin + 127;
     }
     else
     {
-      // если отрицательная полуволна Фазы
+      // if negative half wave phase
       temp_b = 127 - temp_sin;
     }
     // ========================================================
-    // Вычисление амплитуды Фазы C
-    // Умножаем значение из таблицы на коэф. amp_sin и берем старший байт.
+    // calculation of phase amplitude C
+    // multiply the value from the table by coefficient amp_sin and we take the most significant byte.
     temp_sin = (((int16_t)amp_sin * (int16_t)sinus[index_c_sin])) >> 8;
     // --
     if (pol_c)
     {
-      // если положительная полуволна Фазы
+      // if positive half wave phase
       temp_c = temp_sin + 127;
     }
     else
     {
-      // если отрицательная полуволна Фазы
+      // if negative half wave phase
       temp_c = 127 - temp_sin;
     }
     // =========================================================
-    // Запись значений ШИМ в регистры Capture/compare
-    TIM1->CCR1 = temp_a; // PWM CH1 phase_A
-    TIM1->CCR2 = temp_b; // PWM CH2 phase_B
-    TIM1->CCR3 = temp_c; // PWM CH3 phase_C
+    // write pwm values in registers capture/compare
+    TIM1->CCR1 = temp_a; // pwm CH1 phase_A
+    TIM1->CCR2 = temp_b; // pwm CH2 phase_B
+    TIM1->CCR3 = temp_c; // pwm CH3 phase_C
     // =========================================================
-    // Инкремент индексов
+    // index increment
     index_a_sin += inc_ind; // next index phase_A
     index_b_sin += inc_ind; // next index phase_B
     index_c_sin += inc_ind; // next index phase_C
